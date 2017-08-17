aa832b28a1ceadc3c70b6dbe5cc87a60638da72a
Return <31053304+returni@users.noreply.github.com>
2017-08-17 16:03:19 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
   var con=document.getElementById("contain");
   setInterval("echo()",5000);
-  con.click=echo;
+  con.onclick=echo;
   function echo(){
     alert("Welcome To My Web Page"); 
   }
