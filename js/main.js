3fe6f2d3fa62a0eae08d0f6f1cbb6635039c5e52
Return <31053304+returni@users.noreply.github.com>
2017-09-05 16:15:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ window.onload=function(){
 	var audio=document.getElementsByTagName("audio");
 	document.getElementById("img").onclick=welcome;
 	audio[0].play();
-	(audio[0].paused)?(audio[1].play()):(audio[0].play());
+	setTimeout(function(){
+		if(audio[0].paused){audio[1].play()};
+	},3000);
 }
 	function welcome(){
 		alert("Welcome To My Web Page"); 
