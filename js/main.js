5917edf7865ed0aebba55eb60c24e08169ff544e
Return <31053304+returni@users.noreply.github.com>
2017-09-05 11:52:29 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 window.onload=function(){
-  document.getElementById("img").onclick=welcome;
+  document.getElementById("img").onclick=welcome;  
+  var video=document.getElementsByTagName("video");
+  alert(video.length);
+  //setTimeout("autoplay(video)",420000);
 }
   //setInterval("echo()",20000);
   function welcome(){
     alert("Welcome To My Web Page"); 
+  }
+  function autoplay(obj){
+    obj.setAttribute("autoplay","autoplay");
   }
